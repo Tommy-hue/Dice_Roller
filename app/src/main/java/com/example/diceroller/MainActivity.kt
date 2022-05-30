1fcb442732231854bf70ba6db94f2716fc3b0ef5
Tommy-hue <otome1st_class@yahoo.com>
2022-05-31 03:29:15 +0800

    Dice roller app

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
+        // Do a dice roll when the app starts
+        rollDice()
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+            val dice = Dice(6)
+            val diceRoll = dice.roll()
+            val diceRoll2 = dice.roll()
+
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(diceRoll)
+        diceImage2.setImageResource(diceRoll2)
+
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        // Determine which drawable resource ID to use based on the dice roll
+//        return when((1..numSides).random()) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
+        return (1..numSides).random()
+    }
+}
